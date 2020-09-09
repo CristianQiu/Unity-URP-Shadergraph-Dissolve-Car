20ace657b8330472fff22ed949caca4ecec529e5
Cristian Qiu Félez <CristianQiuFelez@gmail.com>
2020-09-10 00:20:52 +0200

    Deleted graphy, improved the code. Baked light again and changed volumes to properly make use of overrides over a global volume. Subtle changes to cameras, postprocessing and URP asset.

---
+++ Assets/Code/CamDissolveControl.cs
@@ -0,0 +1,101 @@
+﻿using Cinemachine;
+using System.Collections;
+using UnityEngine;
+
+public class CamDissolveControl : MonoBehaviour
+{
+    #region Attributes
+
+    [Header("Dissolve")]
+    [SerializeField] private float dissolveSpeed = 0.75f;
+
+    [SerializeField] private MeshRenderer carBodyRenderer = null;
+
+    [Header("Cinemachine cameras")]
+    [SerializeField] private CinemachineVirtualCamera cmCamFront = null;
+
+    [SerializeField] private CinemachineVirtualCamera cmCamBack = null;
+
+    private Material carBodyMat;
+
+    private readonly int dissolveAmountId = Shader.PropertyToID("Dissolve_Progress");
+
+    private float dissolveProgress = 0.0f;
+    private float targetDissolveValue = 0.0f;
+
+    private WaitForSeconds wfsAlternateCams = new WaitForSeconds(5.0f);
+    private Coroutine currDisolveCoroutine;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    private void Start()
+    {
+        carBodyMat = carBodyRenderer.sharedMaterial;
+        carBodyMat.SetFloat(dissolveAmountId, dissolveProgress);
+
+        StartCoroutine(AlternateCams());
+    }
+
+    private void OnValidate()
+    {
+        dissolveSpeed = Mathf.Clamp(dissolveSpeed, 0.0f, dissolveSpeed);
+    }
+
+    #endregion
+
+    #region Coroutines
+
+    private IEnumerator AlternateCams()
+    {
+        while (true)
+        {
+            yield return wfsAlternateCams;
+
+            if (cmCamFront.Priority == 2)
+            {
+                cmCamBack.Priority = 2;
+                cmCamFront.Priority = 1;
+            }
+            else
+            {
+                cmCamFront.Priority = 2;
+                cmCamBack.Priority = 1;
+            }
+        }
+    }
+
+    private IEnumerator DissolveCoroutine()
+    {
+        targetDissolveValue = (targetDissolveValue == 0.0f) ? 1.0f : 0.0f;
+
+        while (Mathf.Abs(targetDissolveValue - dissolveProgress) > 0.0f)
+        {
+            float offsetToTar = targetDissolveValue - dissolveProgress;
+
+            dissolveProgress = dissolveProgress + (Mathf.Sign(offsetToTar) * dissolveSpeed * Time.deltaTime);
+            dissolveProgress = Mathf.Clamp(dissolveProgress, 0.0f, 1.0f);
+
+            carBodyMat.SetFloat(dissolveAmountId, dissolveProgress);
+
+            yield return null;
+        }
+
+        yield break;
+    }
+
+    #endregion
+
+    #region UI
+
+    public void OnDissolveButtonPressed()
+    {
+        if (currDisolveCoroutine != null)
+            StopCoroutine(currDisolveCoroutine);
+
+        currDisolveCoroutine = StartCoroutine(DissolveCoroutine());
+    }
+
+    #endregion
+}
